2a624170abac6e51ca4a7c2853b2716dea6c1782
Ms. Wang <767074895@qq.com>
2020-08-19 15:56:07 +0800

    Update umi-test.js
    新增对应的解析

--- packages/test/bin/umi-test.js
+++ packages/test/bin/umi-test.js
@@ -2,17 +2,22 @@
 const { yParser, chalk, createDebug } = require('@umijs/utils');
 const debug = createDebug('umi:test');
 
+// 解析控制台对象
 const args = yParser(process.argv.slice(2), {
+  // 命令别名定义
   alias: {
     watch: ['w'],
     version: ['v'],
   },
+  // 将一下对象的值解析成boolean
   boolean: ['coverage', 'watch', 'version', 'debug', 'e2e'],
+  // 定义默认值
   default: {
     e2e: true,
   },
 });
 
+// 引用bin包
 require('../lib')
   .default(args)
   .catch((e) => {
