8355cfbdfb0d43567410437cba2bdd89049f8047
Ms. Wang <767074895@qq.com>
2020-08-19 15:35:34 +0800

    Update javascript.js
    添加注释

--- packages/test/helpers/transformers/javascript.js
+++ packages/test/helpers/transformers/javascript.js
@@ -1,6 +1,10 @@
+
+// 如果您已经在使用jest-cli，请添加babel-jest，它将使用Babel自动编译JavaScript代码。
 const babelJest = require('babel-jest');
 
+
 module.exports = babelJest.createTransformer({
+  // 指向根目录处具有jest-preset.json或jest-preset.js文件的npm模块
   presets: [require.resolve('@umijs/babel-preset-umi/node')],
   babelrc: false,
   configFile: false,
